0929eff27c13b62f9a75d4c97800f6a0d31b2147
olaplassen <okp.97@hotmail.com>
2018-04-12 12:19:38 +0200

    big calender

--- src/user.js
+++ src/user.js
@@ -13,7 +13,7 @@ import { outlogged } from './app';
 // Then import the virtualized Select HOC
 import VirtualizedSelect from 'react-virtualized-select'
 
-
+BigCalendar.setLocalizer(BigCalendar.momentLocalizer(moment))
 
 export class UserMenu extends React.Component {
   //props for å hente verdien fra brukeren som logget inn
