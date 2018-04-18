a251d17d779540cedb3379d2f244d3d531fb4825
olaplassen <okp.97@hotmail.com>
2018-04-18 23:46:22 +0200

    asd

--- src/components/User/EventInfo/EventInfo.js
+++ src/components/User/EventInfo/EventInfo.js
@@ -181,25 +181,18 @@ export default class EventInfo extends React.Component {
 	componentDidMount() {
 
 		let signedInUser = userService.getSignedInUser();
-<<<<<<< HEAD
 		userService.getInteressedUsers(this.arrangement.id).then((result) => {
 			console.log(result)
 		});
 		userService.getWatchList(this.arrangement.id).then((result) => {
 			this.allWatchList = result;
 			this.forceUpdate();
-=======
-
->>>>>>> 13be54bc47c58db06e44f81ec9e1c79c51ff3f9d
 
 		userService.getArrangementInfo(this.id).then((result) => {
 			this.arrangement = result;
 			this.start = this.arrangement.start.toLocaleString().slice(0, -3);
 			this.end = this.arrangement.end.toLocaleString().slice(0, -3);
 			this.show = this.arrangement.showTime.toLocaleString().slice(0, -3);
-<<<<<<< HEAD
-
-=======
 			if(signedInUser.admin == 1) {
 				userService.getEventRolleinfo(this.arrangement.id).then((result) => {
 					this.eventRoller = result;
@@ -211,7 +204,6 @@ export default class EventInfo extends React.Component {
 				this.forceUpdate();
 	      console.log(this.arrangement.id)
 			})
->>>>>>> 13be54bc47c58db06e44f81ec9e1c79c51ff3f9d
 			userService.getRolesForArr(this.arrangement.id).then((result) => {
 				this.allSelectedRoles = result;
 
@@ -379,26 +371,6 @@ export default class EventInfo extends React.Component {
 				}
 			});
 
-<<<<<<< HEAD
-			userService.getInteressedUsers(this.arrangement.id).then((result) => {
-				var users = [];
-
-				result.forEach(function (user) {
-					users.push(
-						<div>
-							<li key={user.id}>{user.firstname + " "}</li>
-							<select key={user.id} onChange={function(e) {userService.UpsertRoleForArrangement(user.userId, e.target.value)}}>
-								<option value='defaultValue' selected>-- Velg rolle -- </option>
-								{rolesAvailableForArrangement}
-							</select>
-						</div>
-					);
-				});
-				this.state.users = users;
-				this.forceUpdate();
-			});
-		}
-=======
 					getInteressedUsers() {
 
 						var rolesAvailableForArrangement = [];
@@ -427,5 +399,4 @@ export default class EventInfo extends React.Component {
 							this.forceUpdate();
 						});
 					}
->>>>>>> 13be54bc47c58db06e44f81ec9e1c79c51ff3f9d
 }
